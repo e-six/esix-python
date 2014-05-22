f307e04e5cf2bd58a7880e4df1d3ed891ee38500
Alex Schaeffer <AMVPh34r@gmail.com>
2014-05-22 18:12:41 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Copyright (c)2014 Alex Schaeffer
+"""
+
+from setuptools import setup
+import esix
+
+setup(name='esix',
+      version=esix.__version__,
+      description='High level frontend for the e621 JSON API.',
+      url='https://bitbucket.org/AMV_Ph34r/python-esix',
+      author=esix.__author__,
+      author_email='',
+      license='MIT',
+      packages=['esix'])
