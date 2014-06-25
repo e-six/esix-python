798d1ca80b9d2beaacb436a0248bde9a0a24a768
AMV_Ph34r <AMVPh34r@gmail.com>
2014-06-25 10:55:07 -0400

    Added forum module to package init

--- esix/__init__.py
+++ esix/__init__.py
@@ -8,6 +8,7 @@ __author__ = "Alex Schaeffer"
 __copyright__ = "Copyright (c)2014, " + __author__
 
 __all__ = ["api", "file", "config", "errors", "post",
-           "comment", "user", "tag", "pool", "takedown"]
+           "comment", "user", "tag", "pool", "takedown",
+           "forum"]
 
 from . import *
