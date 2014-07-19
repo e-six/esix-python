4dee01b2caf2a72d81da94f2e4bfce5d324bbd59
AMV_Ph34r <AMVPh34r@gmail.com>
2014-07-19 00:10:03 -0400

    Added property for post page URLs

--- esix/post.py
+++ esix/post.py
@@ -380,6 +380,11 @@ class Post(object):
     @preview_height.setter
     def preview_height(self, value):
         self._data['preview_height'] = value
+    
+    @property
+    def post_url(self):
+        """Returns the site URL the post can be found at."""
+        return config.BASE_URL + 'post/show/' + str(self.id)
 
     @property
     def favorited_users(self):
