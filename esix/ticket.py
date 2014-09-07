20d702b312b94519b33437ac6d81352d2be4f6b7
AMV_Ph34r <AMVPh34r@gmail.com>
2014-09-07 15:53:33 -0400

    Added all possible properties to Ticket class

--- esix/ticket.py
+++ esix/ticket.py
@@ -59,11 +59,108 @@ class Ticket(object):
 
     @property
     def id(self):
-        """Returns the ID number of the ticket."""
         return self._data['id']
     @id.setter
     def id(self, value):
         self._data['id'] = value
+
+    @property
+    def type(self):
+        return self._data['type']
+    @type.setter
+    def type(self, value):
+        self._data['type'] = value
+
+    @property
+    def status(self):
+        return self._data['status']
+    @status.setter
+    def status(self, value):
+        self._data['status'] = value
+
+    @property
+    def user(self):
+        return self._data['user']
+    @user.setter
+    def user(self, value):
+        self._data['user'] = value
+
+    @property
+    def username(self):
+        return self._data['username']
+    @username.setter
+    def username(self, value):
+        self._data['username'] = value
+
+    @property
+    def created_at(self):
+        return self._data['created_at']
+    @created_at.setter
+    def created_at(self, value):
+        self._data['created_at'] = value
+
+    @property
+    def updated_at(self):
+        return self._data['updated_at']
+    @updated_at.setter
+    def updated_at(self, value):
+        self._data['updated_at'] = value
+
+    @property
+    def desired_username(self):
+        return self._data['desired_username']
+    @desired_username.setter
+    def desired_username(self, value):
+        self._data['desired_username'] = value
+
+    @property
+    def oldname(self):
+        return self._data['oldname']
+    @oldname.setter
+    def oldname(self, value):
+        self._data['oldname'] = value
+
+    @property
+    def reason(self):
+        return self._data['reason']
+    @reason.setter
+    def reason(self, value):
+        self._data['reason'] = value
+
+    @property
+    def reported_comment(self):
+        return self._data['reported_comment']
+    @reported_comment.setter
+    def reported_comment(self, value):
+        self._data['reported_comment'] = value
+
+    @property
+    def handled_by(self):
+        return self._data['handled_by']
+    @handled_by.setter
+    def handled_by(self, value):
+        self._data['handled_by'] = value
+
+    @property
+    def handled_by_name(self):
+        return self._data['handled_by_name']
+    @handled_by_name.setter
+    def handled_by_name(self, value):
+        self._data['handled_by_name'] = value
+
+    @property
+    def reported_forum(self):
+        return self._data['reported_forum']
+    @reported_forum.setter
+    def reported_forum(self, value):
+        self._data['reported_forum'] = value
+
+    @property
+    def response(self):
+        return self._data['response']
+    @response.setter
+    def response(self, value):
+        self._data['response'] = value
 
     def dump_data(self):
         """Returns a dict of all data stored locally for this object.
