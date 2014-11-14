151ba45d73a2c908f73ea9e3285f20bb6b133737
AMV_Ph34r <AMVPh34r@gmail.com>
2014-11-14 18:39:53 -0500

    Added basic news module

---
+++ esix/news.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+News class for the e621 API.
+"""
+
+from . import api, config, errors, user
+
+
+class News(object):
+    def __init__(self, news_id=None, news_data=None):
+        """Create an instance of a news post.
+
+        :param news_id: The ID number of the tag to fetch online.
+        :type news_id: int
+        :param news_data: Raw data to load directly into the object.
+        :type news_data: dict
+        :raises: errors.NewsNotFoundError
+        """
+        self._data = {}
+        for prop in []:
+            self._data[prop] = None
+        if news_id is not None:
+            try:
+                data = api._fetch_data(
+                    config.BASE_URL + '/news/show.json?id=' + \
+                    str(news_id)
+                )
+                for prop in data: self._data[prop] = data[prop]
+            except errors.JSONError:
+                raise errors.NewsNotFoundError('The requested news post ' +\
+                    'could not be found.')
+        if news_data is not None:
+            for prop in news_data: self._data[prop] = news_data[prop]
+
+    @property
+    def url(self):
+        """Returns the site URL the news post can be found at."""
+        return config.BASE_URL + 'news/show/' + str(self.id)
+
+    def dump_data(self):
+        """Returns a dict of all data stored locally for this object.
+
+        :returns: All locally-stored news data.
+        :rtype: dict
+        """
+        return self._data
