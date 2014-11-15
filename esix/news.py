75509018ac8b0b78ac2d351650651850b020d4aa
AMV_Ph34r <AMVPh34r@gmail.com>
2014-11-15 14:58:13 -0500

    Added getters/setters for properties

--- esix/news.py
+++ esix/news.py
@@ -33,6 +33,46 @@ class News(object):
             for prop in news_data: self._data[prop] = news_data[prop]
 
     @property
+    def id(self):
+        """Returns the ID number of the news post."""
+        return self._data['id']
+    @id.setter
+    def id(self, value):
+        self._data['id'] = value
+
+    @property
+    def user_id(self):
+        """Returns the ID of the user who submitted the post."""
+        return self._data['user_id']
+    @user_id.setter
+    def user_id(self, value):
+        self._data['user_id'] = value
+    
+    @property
+    def created_at(self):
+        """Returns a dict of when the post was created."""
+        return self._data['created_at']
+    @created_at.setter
+    def created_at(self, value):
+        self._data['created_at'] = value
+    
+    @property
+    def updated_at(self):
+        """Returns a dict of when the post was last updated."""
+        return self._data['updated_at']
+    @updated_at.setter
+    def updated_at(self, value):
+        self._data['updated_at'] = value
+
+    @property
+    def post(self):
+        """Returns the body of the post."""
+        return self._data['post']
+    @post.setter
+    def post(self, value):
+        self._data['post'] = value
+
+    @property
     def url(self):
         """Returns the site URL the news post can be found at."""
         return config.BASE_URL + 'news/show/' + str(self.id)
