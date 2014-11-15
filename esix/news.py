8ce4d03edf1435c43989f68e929235df587422b5
AMV_Ph34r <AMVPh34r@gmail.com>
2014-11-15 14:59:27 -0500

    Removed ability to fetch news by ID, not supported by API

--- esix/news.py
+++ esix/news.py
@@ -7,11 +7,9 @@ from . import api, config, errors, user
 
 
 class News(object):
-    def __init__(self, news_id=None, news_data=None):
+    def __init__(self, news_data=None):
         """Create an instance of a news post.
 
-        :param news_id: The ID number of the tag to fetch online.
-        :type news_id: int
         :param news_data: Raw data to load directly into the object.
         :type news_data: dict
         :raises: errors.NewsNotFoundError
@@ -19,16 +17,6 @@ class News(object):
         self._data = {}
         for prop in ['id', 'user_id', 'created_at', 'updated_at', 'post']:
             self._data[prop] = None
-        if news_id is not None:
-            try:
-                data = api._fetch_data(
-                    config.BASE_URL + '/news/show.json?id=' + \
-                    str(news_id)
-                )
-                for prop in data: self._data[prop] = data[prop]
-            except errors.JSONError:
-                raise errors.NewsNotFoundError('The requested news post ' +\
-                    'could not be found.')
         if news_data is not None:
             for prop in news_data: self._data[prop] = news_data[prop]
 
@@ -71,11 +59,6 @@ class News(object):
     @post.setter
     def post(self, value):
         self._data['post'] = value
-
-    @property
-    def url(self):
-        """Returns the site URL the news post can be found at."""
-        return config.BASE_URL + 'news/show/' + str(self.id)
 
     def dump_data(self):
         """Returns a dict of all data stored locally for this object.
